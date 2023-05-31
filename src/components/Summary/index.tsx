858a138bfa7af994a65d3f2fc3fd168ea0a15455
Murilo Thom <murilothom.2002@hotmail.com>
2023-05-30 21:14:41 -0300

    feat: format prices and dates

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,23 +1,27 @@
-import { SummaryCard, SummaryContainer } from "./styles";
-import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
-import { useContext } from "react";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { SummaryCard, SummaryContainer } from './styles';
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
+import { useContext } from 'react';
+import { TransactionsContext } from '../../contexts/TransactionsContext';
+import { priceFormatter } from '../../utils/formatter';
 
-export function Summary () {
-  const { transactions } = useContext(TransactionsContext)
+export function Summary() {
+  const { transactions } = useContext(TransactionsContext);
 
-  const summary = transactions.reduce((acc, transaction) => {
-    if (transaction.type === 'income') {
-      acc.income += transaction.price
-      acc.total += transaction.price
-    } else {
-      acc.outcome += transaction.price
-      acc.total -= transaction.price
-    }
-    return acc
-  }, { income: 0, outcome: 0, total: 0 })
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+      return acc;
+    },
+    { income: 0, outcome: 0, total: 0 }
+  );
 
-  return(
+  return (
     <SummaryContainer>
       <SummaryCard>
         <header>
@@ -25,7 +29,7 @@ export function Summary () {
           <ArrowCircleUp size={32} color='#00b37e' />
         </header>
 
-        <strong>{summary.income}</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -34,17 +38,17 @@ export function Summary () {
           <ArrowCircleDown size={32} color='#f75a68' />
         </header>
 
-        <strong>{summary.outcome}</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
 
-      <SummaryCard variant="green">
+      <SummaryCard variant='green'>
         <header>
           <span>Total</span>
           <CurrencyDollar size={32} color='#fff' />
         </header>
 
-        <strong>{summary.total}</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
-  )
+  );
 }
